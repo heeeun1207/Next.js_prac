ad6ae39739ce06af62285a5ad6a53166f844da91
jungheeeun <jungheeeun272@gmail.com>
2023-09-24 22:05:42 +0900

    간단하게 화면 구성

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Titel = styled.h1`
+  margin-bottom: 32px;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Label = styled.span`
+  margin: 0 16px;
+  font-size: 1.2rem;
+`;
+
+const Button = styled.button`
+  border: 0;
+  color: #ffffff;
+  background-color: #ff5722;
+  cursor: pointer;
+  padding: 8px 16px;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #ff5722;
+    opacity: 0.8;
+  }
+
+  &:active {
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Titel>Counter App</Titel>
+      <Contents>
+        <Button>-</Button>
+        <Label>0</Label>
+        <Button>+</Button>
+      </Contents>
+    </Container>
   );
 }
 
