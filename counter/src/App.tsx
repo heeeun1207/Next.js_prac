e49143c1099c608b29fb733d1861d2c0ed9588c7
jungheeeun <jungheeeun272@gmail.com>
2023-09-25 11:24:41 +0900

    components/Button 컴포넌트 생성

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { Button } from 'components/Button';
 
 const Container = styled.div`
   height: 100vh;
@@ -9,7 +10,7 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const Titel = styled.h1`
+const Title = styled.h1`
   margin-bottom: 32px;
 `;
 
@@ -24,41 +25,16 @@ const Label = styled.span`
   font-size: 1.2rem;
 `;
 
-const Button = styled.button`
-  border: 0;
-  color: #ffffff;
-  background-color: #bda5c3;
-  cursor: pointer;
-  padding: 8px 16px;
-  border-radius: 4px;
-
-  &:hover {
-    background-color: #bda5c3;
-    opacity: 0.8;
-  }
-
-  &:active {
-    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
-  }
-`;
-
 function App() {
   const [counter, setCounter] = useState(0);
 
-  const sub = () => {
-    setCounter(counter - 1);
-  };
-
-  const add = () => {
-    setCounter(counter + 1);
-  };
   return (
     <Container>
-      <Titel>Counter App</Titel>
+      <Title>Counter App</Title>
       <Contents>
-        <Button onClick={sub}>-</Button>
+        <Button />
         <Label>{counter}</Label>
-        <Button onClick={add}>+</Button>
+        <Button />
       </Contents>
     </Container>
   );
---
+++ counter/src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+const Container = styled.button`
+  border: 0;
+  color: #ffffff;
+  background-color: #bda5c3;
+  cursor: pointer;
+  padding: 8px 16px;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #bda5c3;
+    opacity: 0.8;
+  }
+
+  &:active {
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const Button = () => {
+  return <Container onClick={() => console.log('click')}>test</Container>;
+};
